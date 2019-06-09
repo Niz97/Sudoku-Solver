bfcc559a20d8ebf24bae6d4e599a41d3df375b57
Niz97 <97niran@gmail.com>
2019-06-09 12:05:17 +0100

    Implemented logic to work out possibilities
    getPossibilities() works out values appearing in same row and columns. Next step is to implement logic to work out numbers in same 3x3 grid

--- solver.py
+++ solver.py
@@ -12,6 +12,9 @@ board = [
 
 def main():
 	convertBoard()
+	# printBoard()
+	solve(1,8)
+	print(getValue(0,6))
 
 
 def printBoard():
@@ -19,11 +22,46 @@ def printBoard():
 		print(row)
 
 # convert board so that list contents are 
-# individual characters
+# individual characters rather than a single string
+# doing list(line) will create another nested list [[]]
 def convertBoard():
 	for idx,row in enumerate(board):
 		for line in row:
-			row = list(line)
-			board[idx] = row
+			convertedRow = list(line)
+			board[idx] = convertedRow
+
+
+# checks what numbers exist in a positions
+# respective row, column and 3x3 grid
+def solve(x,y):
+	# row values
+	rowValues = board[y]
+	print("Row:",rowValues)
+
+	colValues = []
+	for i in range(0,9):
+		colValues.append(board[i][x])
+	print("Col:", colValues)
+
+	gridValues = []
+
+
+	# print(getPossibilities(rowValues, colValues))
+
+def getPossibilities(lstA, lstB):
+	# generate list of strings from 1 to 9
+	possibilities = [str(x) for x in range(1,10)]
+
+	# probably better to use sets in the future
+	# https://www.geeksforgeeks.org/python-set-operations-union-intersection-difference-symmetric-difference/ 
+	for i in range(0,9):
+		if lstA[i] in possibilities:
+			possibilities.remove(lstA[i])
+		if lstB[i] in possibilities:
+			possibilities.remove(lstB[i])
+	return possibilities
+
+def getValue(x,y):
+	return board[y][x]
 
 main()
